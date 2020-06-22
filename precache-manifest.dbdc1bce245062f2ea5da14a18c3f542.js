6de851f335e29294da835cab4f81e4f44c45d235
smoshis <azevedoshonda@gmail.com>
2020-06-22 15:50:44 -0400

    Updates

---
+++ precache-manifest.dbdc1bce245062f2ea5da14a18c3f542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dad8944fcb16d1eb5677a23fe8698a",
+    "url": "/shondaMoshisProjectFive/index.html"
+  },
+  {
+    "revision": "18be67efcd4fee277f93",
+    "url": "/shondaMoshisProjectFive/static/css/main.66e35b92.chunk.css"
+  },
+  {
+    "revision": "46e7c044e0dbcfed965d",
+    "url": "/shondaMoshisProjectFive/static/js/2.30605860.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/shondaMoshisProjectFive/static/js/2.30605860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18be67efcd4fee277f93",
+    "url": "/shondaMoshisProjectFive/static/js/main.15993337.chunk.js"
+  },
+  {
+    "revision": "4f6d7204d15b4eb666e0",
+    "url": "/shondaMoshisProjectFive/static/js/runtime-main.2f1721cd.js"
+  }
+]);
