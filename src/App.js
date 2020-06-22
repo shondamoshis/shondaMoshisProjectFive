125ba358f32e2126c2bf8cc0b0addeedf09b631b
smoshis <azevedoshonda@gmail.com>
2020-06-22 11:05:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import firebase from './firebase';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+    userInput:[],
+    inputOne:'',
+    inputTwo:'',
+    inputThree:''
+  }
+}
+
+componentDidMount(){
+  const dbRef = firebase.database().ref();
+
+  dbRef.on('value',(response)=>{
+
+  const newState = [];
+  const data = response.val();
+  
+  for(let key in data){
+    newState.push({item:data[key], id:key})
+  }
+  this.setState({
+    userInput:newState
+  })
+  })
+
+}
+
+handleChange = (event)=>{
+this.setState({
+  [event.target.name]:event.target.value
+})
+}
+handleClick = (e)=>{
+  e.preventDefault();
+
+  const dbRef = firebase.database().ref()
+  
+  dbRef.push(this.state.inputOne)
+  dbRef.push(this.state.inputTwo)
+  dbRef.push(this.state.inputThree)
+
+  this.setState({
+    inputOne:'',
+    inputTwo:'',
+    inputThree:''
+  })
+}
+ 
+  
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App wrapper">
+      <h1>Gratitude</h1>
+      <h2>What is Gratitude?</h2>
+      <p>The quality of being thankful; readiness to show appreciation for and to return kindness.*</p>
+      <h2>Why is it important to practice Gratitude?</h2>
+      <p> People who regularly practice gratitude by taking time to notice and reflect upon the things they're thankful for experience more positive emotions, feel more alive, sleep better, express more compassion and kindness, and even have stronger immune systems.**</p>
+      <h3>Please share three things you are grateful for:</h3>
+      <input type="text" value={this.state.inputOne} name="inputOne" onChange={this.handleChange}/>
+      <input type="text" value={this.state.inputTwo} name="inputTwo" onChange={this.handleChange}/>
+      <input type="text" value={this.state.inputThree} name="inputThree" onChange={this.handleChange}/>
+      <button onClick={this.handleClick}>Add</button>
+  <ul>{
+    this.state.userInput.map((word)=>{
+      return(
+        <li key={word.id}>
+          <p>{word.item}</p>
+        </li>
+      )
+    })
+    }</ul>
+      <footer><p>Designed by <a href="www.shondamoshis.com">Shonda Moshis</a></p></footer>
+    
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAkx6OXoX35t6QZKKJT-lmJIzfSQ8ekPuU",
+    authDomain: "practice-gratitude.firebaseapp.com",
+    databaseURL: "https://practice-gratitude.firebaseio.com",
+    projectId: "practice-gratitude",
+    storageBucket: "practice-gratitude.appspot.com",
+    messagingSenderId: "106297587589",
+    appId: "1:106297587589:web:a0db08247f6a89e64fb6ec"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
